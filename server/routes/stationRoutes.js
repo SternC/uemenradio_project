59bb97a910aeda935c64b3253ef3b030c8ad65ab
Your Name <richardsternchandra@gmail.com>
2025-12-06 21:58:48 +0700

    Removed redundant controllers function.

--- server/routes/stationRoutes.js
+++ server/routes/stationRoutes.js
@@ -1,23 +1,10 @@
 import express from "express";
 import {
-  createStation,
   getStations,
-  getStationById,
-  updateStation,
-  deleteStation,
-  checkStream,
-  proxyStream,
 } from "../controllers/stationController.js";
 
 const router = express.Router();
 
-router.post("/stations", createStation);
 router.get("/stations", getStations);
-router.get("/stations/:id", getStationById);
-router.put("/stations/:id", updateStation);
-router.delete("/stations/:id", deleteStation);
-
-router.get("/stations/check", checkStream);
-router.get("/stations/stream", proxyStream);
 
 export default router;
