840219ff1f430518a0da37ccc6ab288a8b2e9cef
Your Name <richardsternchandra@gmail.com>
2025-12-06 18:42:12 +0700

    Fixed form path.

--- client/main.jsx
+++ client/main.jsx
@@ -5,6 +5,7 @@ import api from "./src/components/Api.jsx";
 
 import Home from "./src/pages/Home.jsx";
 import NotFound from "./src/pages/NotFound.jsx";
+import FormPage from "./src/pages/Form.jsx";
 import "./src/css/style.css";
 
 api.defaults.withCredentials = true;
@@ -17,6 +18,10 @@ const routerPath = createBrowserRouter([
   {
     path: "*",
     element: <NotFound />,
+  },
+  {
+    path: "/form",
+    element: <FormPage />,
   }
 ]);
 
